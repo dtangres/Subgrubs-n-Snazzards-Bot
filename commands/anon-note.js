07e2b258381a23900997dc89f7352fd40aea5bf1
Daniel Angres <gdnlclub@gmail.com>
2025-03-21 12:26:39 -0700

    Updated anon-note permissions

--- commands/anon-note.js
+++ commands/anon-note.js
@@ -7,7 +7,13 @@ module.exports = {
 	async execute(interaction) {
 		try {
 			const guild = await interaction.client.guilds.cache.get(process.env.GUILD_ID);
-			await guild.members.fetch(interaction.user.id);
+			await guild.members.fetch(interaction.user.id)
+				.then(data => {
+					console.log(data.user);
+					/* If (!members.includes(interaction.user)) {
+						throw Error('User not in Serverstuck');
+					}*/
+				});
 			const noteRow = new ActionRowBuilder();
 			const noteBox = new TextInputBuilder()
 				.setCustomId('anonNote_content')
@@ -23,8 +29,14 @@ module.exports = {
 				.addComponents(noteRow);
 			await interaction.showModal(modal);
 		} catch (e) {
-			console.log('Error while showing anon-note modal: ', e);
-			await interaction.reply({ content: 'Sorry, something went wrong. If this error persists, ping Meme or a mod.', ephemeral: true });
+			console.log(e.message);
+			if (e.rawError.message === 'Unknown Member') {
+				console.log('Attempt from outside to use anon-note');
+				await interaction.reply({ content: 'Sorry, you don\'t have permission to use this command.', ephemeral: true });
+			} else {
+				console.log('Error while showing anon-note modal: ', e);
+				await interaction.reply({ content: 'Sorry, something went wrong. If this error persists, ping Meme or a mod.', ephemeral: true });
+			}
 		}
 	},
 };
